3039f80c805934b169ed2c568f27c09ba678b242
Salvatore Barretta <salvatore.barretta@senseisrl.it>
2024-02-02 18:06:09 +0100

    added form validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
 import { ReservationModule } from './reservation/reservation.module';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HomeModule, ReservationModule],
--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -7,7 +8,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css'],
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
   reservationForm: FormGroup = new FormGroup({});
-  onSubmit() {}
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', [Validators.required]],
+      checkOutDate: ['', [Validators.required]],
+      guestName: ['', [Validators.required]],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log('IS VALID');
+    } else {
+      alert("form isn't correctly compiled");
+    }
+  }
 }
+
+/*
+
+constructor > quando instazio nuovo form inietta anche il form builder
+
+
+- on submit > if form is valid ?
+- quando? on init
+- constructor >  form builder
+
+*/
